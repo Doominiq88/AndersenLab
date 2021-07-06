80b2e8698e73f864acd37079d73a71c4d1db1c04
Ivan P <88talem@gmail.com>
2021-07-06 15:47:23 +0300

    fix CSS 2 and 4 locators and Xpath 4

--- tasks.js
+++ tasks.js
@@ -2,12 +2,12 @@ console.log("Hello testing!");
 
 // CSS Selector:
 $$("#orb-search-q")
-$$('#orb-nav-links > ul > li:nth-child(4)')
+$$('.orb-nav-links > ul > li.orb-nav-reeldotcom')
 $$('section.module.module--promo > div > ul > li.media-list__item.media-list__item--1 > div > a')
-$$('#orb-nav-links > ul > li:nth-child(2n)') //- не совсем получилось
+$$('div#orb-nav-links li:nth-child(even):not([class$="hide"])')
 
 // Xpath:
 //input[@id="orb-search-q"]
 //div[@id='orb-nav-links']//li[4]
 //section[@class='module module--promo']//div[@class='module__content']//li[@class='media-list__item media-list__item--1']//a[@class='block-link__overlay-link']
-//div[@class='orb-nav-section orb-nav-links orb-nav-focus']//li[position() mod 2 = 0]
+//div[@class='orb-nav-section orb-nav-links orb-nav-focus']//li[not(contains (@class,'hide'))][position() mod 2 = 0]
